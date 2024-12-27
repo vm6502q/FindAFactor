ad93d266c0db171ff86bfc22230d9bbe0463541a
WrathfulSpatula <stranoj@gmail.com>
2024-12-27 08:45:21 -0500

    Bump version (v0.1.6)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ext_modules = [
 
 setup(
     name='FindAFactor',
-    version='0.1.5',
+    version='0.1.6',
     author='Dan Strano',
     author_email='dan@unitary.fund',
     description='Find any nontrivial factor of a number',
