95219de3414d2af277f3f14b8329d186e498648e
WrathfulSpatula <stranoj@gmail.com>
2024-12-29 19:14:42 -0500

    Document everything

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ext_modules = [
 
 setup(
     name='FindAFactor',
-    version='1.1.3',
+    version='1.1.4',
     author='Dan Strano',
     author_email='dan@unitary.fund',
     description='Find any nontrivial factor of a number',
