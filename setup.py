be7d51741079b693fd685897c340ae9f0eeb1066
WrathfulSpatula <stranoj@gmail.com>
2025-01-02 20:52:27 -0500

    < 1000 LoC in _find_a_factor.cpp

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ext_modules = [
 
 setup(
     name='FindAFactor',
-    version='1.6.3',
+    version='1.6.4',
     author='Dan Strano',
     author_email='dan@unitary.fund',
     description='Find any nontrivial factor of a number',
