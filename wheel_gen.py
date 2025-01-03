03c616ee76b82d11d0d25910dbb4b88cc15ac146
WrathfulSpatula <stranoj@gmail.com>
2025-01-03 13:30:24 -0500

    Wheel factorization up to 13

--- wheel_gen.py
+++ wheel_gen.py
@@ -4,16 +4,19 @@
 
 # Driver Code
 if __name__ == '__main__':
-    primes = [2, 3, 5, 7, 11]
+    # Should only contain the primes in the wheel
+    primes = [2, 3, 5, 7, 11, 13]
 
-    # This is the wheel level primes multiplied out.
+    # This is the set of wheel level primes multiplied out.
     # wheel_size = 2
     # wheel_size = 6 # (2 * 3)
     # wheel_size = 30 # (2 * 3 * 5)
     # wheel_size = 210 # (2 * 3 * 5 * 7)
-    wheel_size = 2310 # (2 * 3 * 5 * 7 * 11)
+    # wheel_size = 2310 # (2 * 3 * 5 * 7 * 11)
+    wheel_size = 30030 # (2 * 3 * 5 * 7 * 11 * 13)
     # Include whatever primes are in the wheel, in wheel_size.
 
+    count = 0
     for i in range(1, wheel_size):
         isNotMultiple = True
         for p in primes:
@@ -23,4 +26,7 @@ if __name__ == '__main__':
         if isNotMultiple:
             # Print the elements in the period
             # that are not small prime multiples.
-            print(i, end=" ")
+            print(i, end=", ")
+            count += 1
+    print()
+    print(count)
