fe4abf9a1c2173e8479c31f4b35059971f0497a7
WrathfulSpatula <stranoj@gmail.com>
2025-01-06 10:06:56 -0500

    Fix Windows

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ ext_modules = [
     Extension(
         '_find_a_factor',
         ["FindAFactor/_find_a_factor.cpp", "FindAFactor/dispatchqueue.cpp"],
-        include_dirs=['FindAFactor/include', 'pybind11/include', '/usr/local/include', '/opt/homebrew/include'],
+        include_dirs=['FindAFactor/include', 'pybind11/include', '/usr/local/include', '/opt/homebrew/include',
+                      (os.environ.get('VCPKG_ROOT') if os.environ.get('VCPKG_ROOT') else 'C:/vcpkg') + '/installed/x64-windows/include'],
         language='c++',
         cxx_std=17,
         extra_compile_args = cpp_args,
