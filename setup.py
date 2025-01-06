4ad11027086f1b2e1f1f3a6f14ced9d74cc74960
WrathfulSpatula <stranoj@gmail.com>
2025-01-06 08:26:23 -0500

    /opt/homebrew/include

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ ext_modules = [
     Extension(
         '_find_a_factor',
         ["FindAFactor/_find_a_factor.cpp", "FindAFactor/dispatchqueue.cpp"],
-        include_dirs=['FindAFactor/include', 'pybind11/include', '/usr/local/include'],
+        include_dirs=['FindAFactor/include', 'pybind11/include', '/usr/local/include', '/opt/homebrew/include'],
         language='c++',
         cxx_std=17,
         extra_compile_args = cpp_args,
