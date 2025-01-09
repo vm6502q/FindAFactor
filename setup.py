91939986ccca9d1e1e05beeb7c49b1e91071d0bf
WrathfulSpatula <stranoj@gmail.com>
2025-01-09 18:13:38 -0500

    Version is semantically 3.4.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ ext_modules = [
 
 setup(
     name='FindAFactor',
-    version='3.3.3',
+    version='3.4.0',
     author='Dan Strano',
     author_email='dan@unitary.fund',
     description='Find any nontrivial factor of a number',
