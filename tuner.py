7c68fb4f4ecf111025a150e03ea0807ffdf1fcf8
WrathfulSpatula <stranoj@gmail.com>
2025-01-14 07:45:03 -0500

    Increase default smoothness bound

--- tuner.py
+++ tuner.py
@@ -12,7 +12,7 @@ primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67
 # Define the optimization objective
 def optimization_objective(to_factor, params):
     # Unpack parameters
-    method, trial_division_level, gear_factorization_level, wheel_factorization_level, smoothness_bound_multiplier, batch_size_multiplier = params
+    trial_division_level, gear_factorization_level, wheel_factorization_level, smoothness_bound_multiplier, batch_size_multiplier = params
 
     # Start timing
     start_time = time.perf_counter()
@@ -20,7 +20,7 @@ def optimization_objective(to_factor, params):
     # Run the factorization function
     result = find_a_factor(
         to_factor,
-        use_congruence_of_squares=(method > 0),
+        use_congruence_of_squares=True,
         node_count=1,
         node_id=0,
         trial_division_level=(1<<int(trial_division_level)),
@@ -49,11 +49,10 @@ def main():
 
     # Define the parameter space for optimization
     param_space = [
-        Integer(0, 1, name="use_congruence_of_squares"),          # Enumeration of bool
         Integer(12, 24, name="trial_division_level"),             # Range for trial division level
         Integer(4, 6, name="gear_factorization_level"),           # Gear factorization level
         Integer(4, 5, name="wheel_factorization_level"),          # Wheel factorization level
-        Real(-1.0, 1.0, name="smoothness_bound_multiplier"),      # Smoothness bound multiplier
+        Real(0.0, 3.0, name="smoothness_bound_multiplier"),       # Smoothness bound multiplier
         Real(10.0, 15.0, name="batch_size_multiplier")            # Batch size multiplier
     ]
 
@@ -67,12 +66,11 @@ def main():
 
     # Print the results
     print("Optimal Parameters:")
-    print(f"Use Congruence of Squaes: {(result.x[0] > 0)}")
-    print(f"Trial Division Level: {(1<<result.x[1])}")
-    print(f"Gear Factorization Level: {primes[result.x[2]]}")
-    print(f"Wheel Factorization Level: {primes[result.x[3]]}")
-    print(f"Smoothness Bound Multiplier: {2**result.x[4]}")
-    print(f"Batch Size Multiplier: {2**(result.x[5]-result.x[2])}")
+    print(f"Trial Division Level: {(1<<result.x[0])}")
+    print(f"Gear Factorization Level: {primes[result.x[1]]}")
+    print(f"Wheel Factorization Level: {primes[result.x[2]]}")
+    print(f"Smoothness Bound Multiplier: {2**result.x[3]}")
+    print(f"Batch Size Multiplier: {2**(result.x[4]-result.x[1])}")
     print(f"Minimum Factorization Time: {result.fun:.4f} seconds")
 
     return 0
