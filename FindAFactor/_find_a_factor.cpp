c07735e8a5f86b46822e9395f9975372fac34321
WrathfulSpatula <stranoj@gmail.com>
2025-01-25 15:45:18 -0500

    Debug and simplify

--- FindAFactor/_find_a_factor.cpp
+++ FindAFactor/_find_a_factor.cpp
@@ -763,9 +763,6 @@ struct Factorizer {
   BigInteger wheelRadius;
   size_t wheelEntryCount;
   size_t wheelLevel;
-  size_t smoothBatchLimit;
-  size_t batchSizeVariance;
-  size_t ladderMultiple;
   size_t rowLimit;
   bool isIncomplete;
   std::vector<size_t> primes;
@@ -775,10 +772,10 @@ struct Factorizer {
   ForwardFn forwardFn;
   ForwardFn backwardFn;
 
-  Factorizer(const BigInteger &tfsqr, const BigInteger &tf, const BigInteger &tfsqrt, const BigInteger &range, size_t nodeCount, size_t nodeId, size_t w, size_t wl, size_t spl,
-             size_t rl, size_t bsv, size_t lm, size_t bn, const std::vector<size_t> &p, ForwardFn ffn, ForwardFn bfn)
+  Factorizer(const BigInteger &tfsqr, const BigInteger &tf, const BigInteger &tfsqrt, const BigInteger &range, size_t nodeCount, size_t nodeId, size_t w, size_t wl, size_t rl,
+             size_t bn, const std::vector<size_t> &p, ForwardFn ffn, ForwardFn bfn)
     : dis(0ULL, -1ULL), toFactorSqr(tfsqr), toFactor(tf), toFactorSqrt(tfsqrt), batchRange(range), batchNumber(bn), batchOffset(nodeId * range), batchTotal(nodeCount * range),
-    wheelRadius(1U), wheelEntryCount(w), wheelLevel(wl), smoothBatchLimit(spl), batchSizeVariance(bsv), ladderMultiple(lm), rowLimit(rl), isIncomplete(true), primes(p),
+    wheelRadius(1U), wheelEntryCount(w), wheelLevel(wl), rowLimit(rl), isIncomplete(true), primes(p),
     forwardFn(ffn), backwardFn(bfn)
   {
     for (size_t i = 0U; i < primes.size(); ++i) {
@@ -786,8 +783,8 @@ struct Factorizer {
       wheelRadius *= p;
       sqrPrimes.push_back(p * p);
     }
-    smoothNumberKeys.reserve(smoothBatchLimit);
-    smoothNumberValues.reserve(smoothBatchLimit);
+    smoothNumberKeys.reserve(rowLimit);
+    smoothNumberValues.reserve(rowLimit);
   }
 
   BigInteger getNextAltBatch() {
@@ -828,7 +825,7 @@ struct Factorizer {
   void sievePolynomials(const BigInteger& low, const BigInteger& high) {
     std::vector<BigInteger> smooth_candidates;
     const BigInteger maxLcv = toFactorSqrt + high;
-    for (BigInteger y = toFactorSqrt; y < maxLcv; y += wheelLevel) {
+    for (BigInteger y = toFactorSqrt + 1U; y < maxLcv; y += wheelLevel) {
       // Make the candidate NOT a multiple on the wheels.
       BigInteger candidate = forwardFn(backwardFn(y * y - toFactor));
       // This actually just goes ahead and FORCES
@@ -1048,8 +1045,8 @@ struct Factorizer {
 };
 
 std::string find_a_factor(std::string toFactorStr, size_t method, size_t nodeCount, size_t nodeId, size_t trialDivisionLevel, size_t gearFactorizationLevel,
-                          size_t wheelFactorizationLevel, double smoothnessBoundMultiplier, double batchSizeMultiplier, size_t batchSizeVariance, size_t ladderMultiple,
-                          size_t gaussianEliminationRowMultiplier, std::string sievingBoundStr, bool skipTrialDivision) {
+                          size_t wheelFactorizationLevel, double smoothnessBoundMultiplier, size_t gaussianEliminationRowMultiplier, std::string sievingBoundStr,
+                          bool skipTrialDivision) {
   // Validation section
   if (method > 1U) {
     std::cout << "Mode number " << method << " not implemented. Defaulting to FACTOR_FINDER." << std::endl;
@@ -1168,9 +1165,8 @@ std::string find_a_factor(std::string toFactorStr, size_t method, size_t nodeCou
   // This manages the work of all threads.
   Factorizer worker(toFactor * toFactor, toFactor, fullMaxBase,
                     nodeRange, nodeCount, nodeId,
-                    wheelEntryCount, gearFactorizationLevel, (size_t)(batchSizeMultiplier * smoothPrimes.size() * log(smoothPrimes.size())),
-                    gaussianEliminationRowMultiplier * smoothPrimes.size(), batchSizeVariance,
-                    ladderMultiple, batchStart, smoothPrimes, forward(SMALLEST_WHEEL), backwardFn);
+                    wheelEntryCount, gearFactorizationLevel, (size_t)(gaussianEliminationRowMultiplier * smoothPrimes.size()),
+                    batchStart, smoothPrimes, forward(SMALLEST_WHEEL), backwardFn);
   // Square of count of smooth primes, for FACTOR_FINDER batch multiplier base unit, was suggested by Lyra (OpenAI GPT)
 
   if (isFactorFinder) {
