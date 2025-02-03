725c7f4119dde440843b089cfb7e1b20f565dc65
WrathfulSpatula <stranoj@gmail.com>
2025-02-03 15:36:14 -0500

    Wheels up to 17 for PRIME_PROVER

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ ext_modules = [CMakeExtension('_find_a_factor')]
 
 setup(
     name='FindAFactor',
-    version='6.4.3',
+    version='6.5.0',
     author='Dan Strano',
     author_email='stranoj@gmail.com',
     description='Find any nontrivial factor of a number',
--- wheel_gen.py
+++ wheel_gen.py
@@ -5,19 +5,20 @@
 # Driver Code
 if __name__ == '__main__':
     # Should only contain the primes in the wheel
-    primes = [2, 3, 5, 7, 11]
+    primes = [2, 3, 5, 7, 11, 13, 17]
 
     # This is the set of wheel level primes multiplied out.
     # wheel_size = 2
     # wheel_size = 6 # (2 * 3)
     # wheel_size = 30 # (2 * 3 * 5)
     # wheel_size = 210 # (2 * 3 * 5 * 7)
-    wheel_size = 2310 # (2 * 3 * 5 * 7 * 11)
+    # wheel_size = 2310 # (2 * 3 * 5 * 7 * 11)
     # wheel_size = 30030 # (2 * 3 * 5 * 7 * 11 * 13)
+    wheel_size = 510510 # (2 * 3 * 5 * 7 * 11 * 13 * 17)
     # Include whatever primes are in the wheel, in wheel_size.
 
     count = 0
-    for i in range(1, wheel_size):
+    for i in range(1, wheel_size + 1):
         isNotMultiple = True
         for p in primes:
             if (i % p) == 0:
